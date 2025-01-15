6a7bbea39a27490f84435397bc4b0ab74d162f83
Austin Nelson <austinn243@gmail.com>
2025-01-14 21:37:22 -0700

    [style]: Improve code style for day 2
    Adjusted the formatting and documentation for day 2 to adhere
    to guidelines.

--- day2/main.py
+++ day2/main.py
@@ -21,7 +21,10 @@ def read_spreadsheet(file_path: str) -> Spreadsheet:
         return [[int(cell) for cell in row.split()] for row in file]
 
 
-def summarize_spreadsheet(spreadsheet: Spreadsheet, process_row: Callable[[list[int]], int]) -> int:
+def summarize_spreadsheet(
+    spreadsheet: Spreadsheet,
+    process_row: Callable[[list[int]], int],
+) -> int:
     """Summarize a spreadsheet using a row processing function."""
 
     return sum(process_row(row) for row in spreadsheet)
@@ -34,12 +37,12 @@ def difference_between_extrema(row: list[int]) -> int:
 
 
 def evenly_divisible_quotient(row: list[int]) -> int:
-    """Calculate the quotient of the only two numbers in a row that evenly divide each other."""
+    """Divide the only two numbers in a row that evenly divide each other."""
 
     for dividend, divisor in product(row, repeat=2):
         if dividend != divisor and dividend % divisor == 0:
             return dividend // divisor
-        
+
     return 0
 
 
@@ -54,9 +57,12 @@ def main() -> None:
     checksum = partial(summarize_spreadsheet, process_row=difference_between_extrema)
     print(checksum(spreadsheet))
 
-    sum_evenly_divisible_quotients = partial(summarize_spreadsheet, process_row=evenly_divisible_quotient)
+    sum_evenly_divisible_quotients = partial(
+        summarize_spreadsheet,
+        process_row=evenly_divisible_quotient,
+    )
     print(sum_evenly_divisible_quotients(spreadsheet))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
